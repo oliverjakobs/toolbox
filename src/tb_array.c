9d57a68dfdc3e3e82d4c42b5423472290aa796fb
oliverjakobs <31634843+oliverjakobs@users.noreply.github.com>
2021-05-21 01:23:19 +0200

    renamed buf to arr in tb_array

--- src/tb_array.c
+++ src/tb_array.c
@@ -3,58 +3,63 @@
 #include <stdio.h>
 #include <string.h>
 
-void* tb_array__resize(void* buf, size_t elem_size, size_t new_cap, int shrink)
+/*
+ * if arr == NULL and new_cap == 0 -> new_cap = 1 (first initialization)
+ * if arr != NULL and new_cap < old_cap and not shrink -> no need to resize
+ * if realloc failed return NULL
+ */
+void* tb_array__resize(void* arr, size_t elem_size, size_t new_cap, int shrink)
 {
-    if (!shrink && buf && new_cap < tb_array__cap(buf)) return buf;
-    if (!buf && new_cap == 0) new_cap = 1;
+    if (arr && new_cap < tb_array__cap(arr) && !shrink) return arr;
+    if (!arr && new_cap == 0) new_cap = 1;
 
-    size_t* hdr = realloc(buf ? tb_array__hdr(buf) : NULL, TB_ARRAY_HDR_SIZE + (new_cap * elem_size));
+    size_t* hdr = realloc(arr ? tb_array__hdr(arr) : NULL, TB_ARRAY_HDR_SIZE + (new_cap * elem_size));
     printf("Realloc %d\n", new_cap);
 
     if (!hdr) return NULL; /* out of memory */
 
     hdr[0] = new_cap;
-    if (!buf) hdr[1] = 0;
+    if (!arr) hdr[1] = 0;
 
     return hdr + 2;
 }
 
-void* tb_array__insert(void* buf, size_t elem_size, void* value, int (*cmp)(const void*,const void*))
+void* tb_array__insert(void* arr, size_t elem_size, void* value, int (*cmp)(const void*,const void*))
 {
     /* array needs to grow */
-    if (!buf || tb_array__len(buf) >= tb_array__cap(buf))
+    if (!arr || tb_array__len(arr) >= tb_array__cap(arr))
     {
-        buf = tb_array__resize(buf, tb_array__growth(buf), elem_size, 0);
-        if (!buf) return NULL;
+        arr = tb_array__resize(arr, tb_array__growth(arr), elem_size, 0);
+        if (!arr) return NULL;
     }
 
     /* find location for the new element */
     size_t index = 0;
-    if (buf && cmp)
+    if (arr && cmp)
     {
-        while (index < tb_array__len(buf) && cmp(((char*)buf) + index, value) < 0)
+        while (index < tb_array__len(arr) && cmp(((char*)arr) + index, value) < 0)
             ++index;
 
         /* move entries back to make space for new element if index is not at the end */
-        if (index < tb_array__len(buf))
+        if (index < tb_array__len(arr))
         {
-            size_t size = (tb_array__len(buf) - index) * elem_size;
-            memcpy(((char*)buf) + (index + 1), ((char*)buf) + index, size);
+            size_t size = (tb_array__len(arr) - index) * elem_size;
+            memcpy(((char*)arr) + (index + 1), ((char*)arr) + index, size);
         }
     }
 
-    tb_array__len(buf)++;
-    return memcpy(((char*)buf) + index, value, elem_size);
+    tb_array__len(arr)++;
+    return memcpy(((char*)arr) + index, value, elem_size);
 }
 
-void* tb_array__remove(void* buf, size_t elem_size, size_t index)
+void* tb_array__remove(void* arr, size_t elem_size, size_t index)
 {
-    if (buf && tb_array__len(buf) > 0)
+    if (arr && tb_array__len(arr) > 0)
     {
-        size_t size = (tb_array__len(buf) - (index + 1)) * elem_size;
-        memcpy(((char*)buf) + index, ((char*)buf) + (index + 1), size);
+        size_t size = (tb_array__len(arr) - (index + 1)) * elem_size;
+        memcpy(((char*)arr) + index, ((char*)arr) + (index + 1), size);
 
-        tb_array__len(buf)--;
+        tb_array__len(arr)--;
     }
-    return buf;
+    return arr;
 }
